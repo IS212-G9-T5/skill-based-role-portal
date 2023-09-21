e3f0faa1a5ba50349c0d58976f5d9f85fa80841b
ZheTao <zacharytao99@gmail.com>
2023-09-22 02:13:43 +0800

    feat:create role listing form
    Create Role Listing form with title, description and skills

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,22 @@ import { useRoutes } from "react-router-dom";
 import routes from "./routes"
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import RolelistingForm from "./pages/hr/signup"
 
+import "./App.css"
 
-const App = ({children}) =>
-  {
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      {children}
-    const content = useRoutes(routes)
-    return content
-    </LocalizationProvider>
-  }
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="App">
+        <RolelistingForm />
+      </div>
+    </>
+  )
+}
 
 export default App
