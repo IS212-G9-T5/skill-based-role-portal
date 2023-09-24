43f549b22ae2a2ec539cd2db105e19aa53268efc
Christian Koh <ckoh.2021@scis.smu.edu.sg>
2023-09-24 19:27:43 +0800

    feat: integration of view individual role listing with backend
    Overview:
    - Individual Role Listing now fetches from the database and shows the details accordingly, although the endpoint currently is fixed for the Job with ID 25
    - Page now shows the Start Date and End Date of the role
    -

--- frontend/src/pages/applicant/main.tsx
+++ frontend/src/pages/applicant/main.tsx
@@ -2,23 +2,37 @@ import { useEffect, useState } from "react"
 
 import NavBar from "../../components/Navbar"
 import RoleListing from "./RoleListing"
+import { useEffect, useState } from "react"
+
+type apiRoleDataProps = {
+  roleID: string
+  roleName: string
+  roleDescription: string
+  roleStartTime: string
+  roleEndTime: string
+  status: boolean
+  skillsRequired: string[]
+}
 
 const ViewRoleListing = () => {
-  const [apiRoleData, setApiRoleData] = useState<Roles[]>([])
-  const endpointUrl = "http://127.0.0.1:5000/api/listings/25"
+
+  const [apiRoleData, setApiRoleData] = useState<apiRoleDataProps[]>([]);
+  const endpointUrl = "http://localhost:5000/api/listings/25";
 
   useEffect(() => {
     fetch(endpointUrl)
-      .then((response) => response.json())
-      .then((res) => {
-        if (Array.isArray(res.data)) {
-          setApiRoleData(res.data)
-        } else if (typeof res.data === "object") {
-          setApiRoleData([res.data])
-        }
-      })
-      .catch((error) => console.log(error))
-  }, [])
+    .then((response) => response.json())
+    .then(res => {
+      if (Array.isArray(res.data)) {
+        setApiRoleData(res.data);
+      } else if (typeof res.data === 'object') {
+        // If it's a single object, wrap it in an array
+        console.log(res.data);
+        setApiRoleData([res.data]);
+      }
+    })
+    .catch((error) => console.log(error))
+  }, []);
 
   const title = "SKILLS BASED ROLE PORTAL"
   const items = ["View Listings", "View Profile", "Logout"]
@@ -28,14 +42,14 @@ const ViewRoleListing = () => {
       <NavBar title={title} items={items} />
       {apiRoleData.map((roleData) => (
         <RoleListing
-          key={roleData.id}
-          id={roleData.id}
-          name={roleData["role"].name}
-          description={roleData["role"].description}
-          start_date={roleData.start_date}
-          end_date={roleData.end_date}
+          key={roleData.id} // Provide a unique key for each RoleListing
+          roleID={roleData.id}
+          roleName={roleData["role"].name}
+          roleDescription={roleData["role"].description}
+          roleStartTime={roleData.start_date}
+          roleEndTime={roleData.end_date}
           status={roleData.status}
-          skills={roleData["role"].skills}
+          skillsRequired={roleData["role"].skills}
         />
       ))}
     </div>
