7929631daabcc08ecc026db4c5524cf107326abe
ZheTao <zacharytao99@gmail.com>
2023-09-25 03:37:30 +0800

    feat: draft role listing form
    Role listing form contains role field, description textarea, start and end date and skills checkbox

---
+++ frontend/src/components/Select/index.tsx
@@ -0,0 +1,51 @@
+import TextField from '@mui/material/TextField';
+import MenuItem from "@mui/material/MenuItem";
+import { FormikTouched, useField, useFormikContext } from "formik";
+import React from "react";
+
+interface SelectWrapperProps {
+    name: string,
+    label: string,
+    options: string,
+    error: boolean,
+    helperText: FormikTouched<any>
+}
+const SelectWrapper = ({
+        name,
+        options,
+        ...otherProps }: SelectWrapperProps) => {
+    const { setFieldValue } = useFormikContext()
+    const [field] = useField(name)
+
+
+    const handleChange = evt => {
+        const {value} = evt.target
+        setFieldValue(name, value)
+
+    }
+
+    const configSelect = {
+        ...field,
+        ...otherProps,
+        select:true,
+        fullWidth: true,
+        onChange: handleChange
+    }
+
+
+
+    return (
+        <TextField {...configSelect}>
+            {Object.keys(options).map((item,pos) =>
+            {
+                return (
+                  <MenuItem key={pos} value={item}>
+                      {options[item]}
+                  </MenuItem>
+                )
+
+            })}
+        </TextField>
+    )
+}
+export default SelectWrapper
