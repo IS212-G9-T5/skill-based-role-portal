e713df1d74a1a0e3497d18de1f5de38719d116a5
Christian Koh <111347751+christianmk04@users.noreply.github.com>
2023-09-24 20:42:07 +0800

    feat: Backend Integration with View Individual Role Listings Page  (#6)
    Co-authored-by: zhiweit <theanzhiwei@gmail.com>

--- backend/application/services/role_listing_service.py
+++ backend/application/services/role_listing_service.py
@@ -1,16 +1,25 @@
 from typing import List, Optional
 from application.models.role_listing import RoleListing
 from application.extensions import db
+from flask_sqlalchemy.pagination import Pagination
+from sqlalchemy.orm import joinedload
 
 
-def find_all() -> List[RoleListing]:
-    listings = db.session.execute(db.select(RoleListing)).scalars().all()
+def find_all_paginated(page: int, page_size: int) -> Pagination:
+    # listings = db.session.execute(db.select(RoleListing)).scalars().all() # without pagination
+
+    listings = (
+        RoleListing.query.order_by(RoleListing.end_date.desc())
+        .options(joinedload(RoleListing.role))  # eager load the role object
+        .paginate(page=page, per_page=page_size, error_out=False)
+    )
+
     return listings
 
 
-def find_by_name(name: str) -> Optional[RoleListing]:
+def find_by_id(id: int) -> Optional[RoleListing]:
     listing = (
-        db.session.execute(db.select(RoleListing).where(RoleListing.name == name))
+        db.session.execute(db.select(RoleListing).where(RoleListing.id == id))
         .scalars()
         .first()
     )
