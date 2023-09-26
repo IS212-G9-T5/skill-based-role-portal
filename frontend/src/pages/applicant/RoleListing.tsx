c74c724e000a8b5dae58c77f1ec80279ab538c4c
Wei Lun Teo <weilun.teo.2021@scis.smu.edu.sg>
2023-09-26 10:17:36 +0800

    trying to merge conflicts

--- frontend/src/pages/applicant/RoleListing.tsx
+++ frontend/src/pages/applicant/RoleListing.tsx
@@ -1,45 +1,14 @@
 import { Button, Chip, Grid, Typography } from "@mui/material"
 
-interface RoleListingProps {
-  roleID: string
-  roleName: string
-  roleDescription: string
-  roleStartTime: string
-  roleEndTime: string
-  status: boolean
-  skillsRequired: string[]
-}
-
-const RoleListing = (props: RoleListingProps) => {
+const RoleListing = (props: Roles) => {
   return (
     <div>
-      <div className="pl-[10%] pr-[10%] pt-[2%]">
-        <Typography variant="h2">{props.roleName}</Typography>
-        <Typography variant="h6" className="mb-[2%] text-[#B0B0B4]">
-          Role ID: {props.roleID} | Role Listing Status: {props.status} | Start Date: {props.roleStartTime} | End Date: {props.roleEndTime}
-        </Typography>
-
-        <Typography variant="h5" gutterBottom style={{ marginTop: "3%" }}>
-          <b>
-            <span className="mr-2 bg-[#1976D2] pl-2"></span>
-            Role Description
-          </b>
-        </Typography>
-
-        <Typography variant="body1" style={{ marginBottom: "3%" }} paragraph>
-          {props.roleDescription}
-        </Typography>
-
-        <Grid item xs={12} style={{ marginBottom: "3%" }}>
-          <Typography variant="h5">
-            <strong>
-              <span className="mr-2 bg-[#1976D2] pl-2"></span>
-              Skills Required
-            </strong>
-            <br></br>
-            {props.skillsRequired.map((skill, index) => (
-              <Chip key={index} label={skill} className="mr-[1%] mt-[1%]" />
-            ))}
+      <div>
+        <div className="pl-[10%] pr-[10%] pt-[2%]">
+          <Typography variant="h2">{props.name}</Typography>
+          <Typography variant="h6" className="mb-[2%] text-[#B0B0B4]">
+            Role ID: {props.id} | Role Listing Status: {props.status} | Start
+            Date: {props.start_date} | End Date: {props.end_date}
           </Typography>
 
           <Typography variant="h5" gutterBottom style={{ marginTop: "3%" }}>
@@ -90,4 +59,4 @@ const RoleListing = (props: RoleListingProps) => {
   )
 }
 
-export default RoleListing
+export default RoleListing;
