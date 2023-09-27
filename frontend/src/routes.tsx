3f6973ebd67a6ee5889d5a1353564f8dd81d7f87
Christian Koh <ckoh.2021@scis.smu.edu.sg>
2023-09-27 10:41:02 +0800

    feat: Prototype Update Listings Page
    created a prototype update listings page for testing

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,6 +2,7 @@ import type { RouteObject } from "react-router"
 import AllRoleListing from "./pages/applicant/AllRoleListing"
 import ViewRoleListing from "./pages/applicant/main"
 import CreateRoleListing from "./pages/hr/CreateRoleListing"
+import UpdateRoleListing from "./pages/hr/UpdateRoleListing"
 
 const routes: RouteObject[] = [
   {
@@ -15,6 +16,10 @@ const routes: RouteObject[] = [
   {
     path: "create-role-listing",
     element: <CreateRoleListing />,
+  },
+  {
+    path: "update-role-listing",
+    element: <UpdateRoleListing />,
   },
 ]
 
