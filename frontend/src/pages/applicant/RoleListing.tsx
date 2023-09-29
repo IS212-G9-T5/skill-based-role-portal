6ed18cd4a28c28285c19b9d805811f133030b97f
Wei Lun Teo <weilun.teo.2021@scis.smu.edu.sg>
2023-09-29 21:47:53 +0800

    modify start & end date to closing date only

--- frontend/src/pages/applicant/RoleListing.tsx
+++ frontend/src/pages/applicant/RoleListing.tsx
@@ -16,8 +16,7 @@ const RoleListing = (props: Roles) => {
             <Typography variant="h4">{props.name}</Typography>
           </strong>
           <Typography variant="subtitle1" className="mb-[2%] text-[#B0B0B4]">
-            Role ID: {props.id} | Status: {props.status} | Start Date:{" "}
-            {props.start_date} | End Date: {props.end_date}
+            Role ID: {props.id} | Status: {props.status} | Closing Date: {props.end_date}
           </Typography>
 
           <Typography variant="h6" gutterBottom style={{ marginTop: "3%" }}>
