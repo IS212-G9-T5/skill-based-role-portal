d0047ba6103f8e50168e3aab23b2c84f049f7065
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2023-09-30 02:50:16 +0800

    feat: authentication + authorisation feature

---
+++ backend/application/routes/route_decorators.py
@@ -0,0 +1,24 @@
+from flask import jsonify
+from functools import wraps
+from application.enums import AccessControlRole
+
+from flask_jwt_extended import (
+    verify_jwt_in_request,
+    get_jwt,
+)
+
+
+def admin_required():
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            verify_jwt_in_request()
+            claims = get_jwt()
+            if claims[AccessControlRole.ADMIN.value]:
+                return fn(*args, **kwargs)
+            else:
+                return jsonify(msg="Admin Rights Required"), 402
+
+        return decorator
+
+    return wrapper
