48b9cda909eeeb1e9070d653fcb57e8822888687
zhiweit <theanzhiwei@gmail.com>
2023-10-01 20:28:55 +0800

    feat: search listing by role and filter by skills

--- backend/application/routes/role_listing_route.py
+++ backend/application/routes/role_listing_route.py
@@ -1,14 +1,26 @@
+from typing import List
 from flask import abort, json, jsonify, request, current_app as app
 from marshmallow import ValidationError
 
 from application.models.role_listing import RoleListing
+from application.models.role import Role
+
 # from application.dto.role_listing import UpdateRoleListingDTO
-from application.dto.role_listing import RoleListingDTO, UpdateRoleListingDTO
+from application.dto.role_listing import (
+    RoleListingDTO,
+    RoleListingSkillMatchDTO,
+    UpdateRoleListingDTO,
+)
 from application.services import staff_service
 from . import api
-from application.services import role_listing_service, role_service
+from application.services import role_listing_service, role_service, staff_service
 from application.dto.response import ResponseBodyJSON
 from application.enums import RoleStatus
+from application.extensions import db
+from sqlalchemy import select, text
+
+from flask_sqlalchemy.pagination import SelectPagination
+
 
 DEFAULT_PAGE_SIZE = 10
 
@@ -26,19 +38,53 @@ def find_all_listings_paginated():
     page_size = request.args.get("size", DEFAULT_PAGE_SIZE, type=int)
     page_size = page_size if page_size > 0 else DEFAULT_PAGE_SIZE
 
-    listings = role_listing_service.find_all_paginated(page=page, page_size=page_size)
+    role = request.args.get("role") or ""
+    skills_to_filter = request.args.getlist("skills")
+    app.logger.info(f"GET /listings with params: {request.args}")
 
-    data = {
-        "page": listings.page,
-        "size": listings.per_page,
-        "items": [l.json() for l in listings.items] if listings.items else [],
-        "total": listings.total,
-        "pages": listings.pages,
-        "has_prev": listings.has_prev,
-        "has_next": listings.has_next,
+    # FIXME: get user id from JWT claim to get the user's skills
+    # randomly get a user from the database so simulate a logged in user
+    user = staff_service.find_by_id(3)
+    if user is None:
+        abort(500, description="No user found.")
+
+    user_skills = set([s.name for s in user.skills])
+    app.logger.info(f"user skills: {user_skills}")
+
+    paginated_listings = role_listing_service.find_all_by_role_and_skills_paginated(
+        skills_to_filter=skills_to_filter, role=role, page=page, page_size=page_size
+    )
+
+    data: List[RoleListingSkillMatchDTO] = []
+    for listing in paginated_listings.items:
+        listing_skills = set([s.name for s in listing.role.skills])
+
+        skills_matched = listing_skills.intersection(user_skills)
+        skills_unmatched = listing_skills.difference(user_skills)
+        skills_match_count = len(skills_matched)
+        skills_match_pct = round(skills_match_count / len(listing_skills), 2)
+
+        data.append(
+            RoleListingSkillMatchDTO(
+                listing=listing.json(),
+                skills_matched=list(skills_matched),
+                skills_unmatched=list(skills_unmatched),
+                skills_match_count=skills_match_count,
+                skills_match_pct=skills_match_pct,
+            )
+        )
+
+    res = {
+        "page": paginated_listings.page,
+        "size": paginated_listings.per_page,
+        "items": data,
+        "total": paginated_listings.total,
+        "pages": paginated_listings.pages,
+        "has_prev": paginated_listings.has_prev,
+        "has_next": paginated_listings.has_next,
     }
 
-    return jsonify(data), 200
+    return jsonify(res), 200
 
 
 @api.route("/listings/<int:id>", methods=["GET"])
@@ -83,6 +129,7 @@ def create_listing():
     res = ResponseBodyJSON(data=data.json()).json()
     return jsonify(res), 201
 
+
 @api.route("/listings/<int:id>", methods=["PUT"])
 def update_role_listing(id: int):
     body = request.get_json()
@@ -100,10 +147,20 @@ def update_role_listing(id: int):
 
     # Validate start_time < end_time
     if updated_data["start_date"] >= updated_data["end_date"]:
-        return jsonify({"message": "Invalid start time and end time. Start time must be before the end time."}), 400
+        return (
+            jsonify(
+                {
+                    "message": "Invalid start time and end time. Start time must be before the end time."
+                }
+            ),
+            400,
+        )
 
     # Validate that the status is valid
-    if updated_data["status"] is None or updated_data["status"] not in RoleStatus.__members__:
+    if (
+        updated_data["status"] is None
+        or updated_data["status"] not in RoleStatus.__members__
+    ):
         abort(
             400,
             description=f"Invalid role status: '{id}'. name can only be the following: {RoleStatus.__members__.keys()}",
