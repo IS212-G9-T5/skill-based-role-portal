574c002ad83c20b6cbdc0d7a80a8be15a4b39a15
Bryan Tan Jia Jun <bryan.tanjiajun@gmail.com>
2023-10-02 20:37:41 +0800

    Created apply and withdraw buttons

--- frontend/src/pages/applicant/RoleListing.tsx
+++ frontend/src/pages/applicant/RoleListing.tsx
@@ -7,6 +7,8 @@ import DialogContentText from "@mui/material/DialogContentText"
 import DialogTitle from "@mui/material/DialogTitle"
 import { useNavigate } from "react-router-dom"
 
+
+
 const RoleListing = (props: Roles) => {
   const navigate = useNavigate()
 
@@ -40,6 +42,99 @@ const RoleListing = (props: Roles) => {
   const handleBackToListings = () => {
     navigate(`/all-role-listing`)
   }
+
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
+  const [openApply, setOpenApply] = useState(false);
+  const [openWithdraw, setOpenWithdraw] = useState(false);
+
+
+  const handleApplyOpen = () => {
+    setOpenApply(true);
+  };
+
+  const handleApplyClose = () => {
+    setOpenApply(false);
+  };
+
+  const handleWithdrawOpen = () => {
+    setOpenWithdraw(true);
+  };
+
+  const handleWithdrawClose = () => {
+    setOpenWithdraw(false);
+  };
+
+  const handleApplySubmit = () => {
+    // Handle the submission logic for applying here
+    // This function should be replaced with your actual submission logic
+    console.log('Application submitted');
+    setOpenApply(false);
+  };
+
+  const handleWithdrawSubmit = () => {
+    // Handle the submission logic for withdrawing here
+    // This function should be replaced with your actual withdrawal logic
+    console.log('Application withdrawn');
+    setOpenWithdraw(false);
+  };
+
+
+  const data = {
+    "listing": {
+        "end_date": "2023-10-11",
+        "id": 31,
+        "role": {
+            "description": "lorem ipsum",
+            "name": "Consultant",
+            "skills": [
+                "Collaboration",
+                "Communication",
+                "Learning Agility"
+            ]
+        },
+        "start_date": "2023-09-11",
+        "status": "OPEN"
+    },
+    "skills_match_count": 2,
+    "skills_match_pct": 0.67,
+    "skills_matched": [
+        "Communication",
+        "Collaboration"
+    ],
+    "skills_unmatched": [
+        "Learning Agility"
+    ],
+    "has_applied": false
+}
+  
+  const has_applied = data.has_applied;
+
+  // useEffect(() => {
+  //   const currUrl = window.location.href
+  //   const id = currUrl.split("/").pop()
+  //   const endpointUrl = `http://127.0.0.1:5000/api/listings/${id}`
+  //   fetch(endpointUrl)
+  //   .then((response) => response.json())   
+  //   .then((res) => {
+  //     console.log(res.data)
+  //   })
+  // }, [])
+
+
+
 
   return (
     <div>
@@ -113,13 +208,75 @@ const RoleListing = (props: Roles) => {
             </DialogActions>
           </Dialog>
 
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ marginTop: "20px" }}
-          >
-            Apply Now
-          </Button>
+
+          {
+            has_applied ? (
+              <Button
+                variant="contained"
+                color="secondary"  // You can use 'secondary' for a red color
+                style={{ marginTop: "20px" }}
+                onClick={handleWithdrawOpen}
+                // onClick={() => }
+              >
+                Withdraw Application
+              </Button>
+            ) : (
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ marginTop: "20px" }}
+                onClick={handleApplyOpen}
+              >
+                Apply Now
+              </Button>
+            )
+          }
+
+
+      <Modal
+        open={openApply}
+        onClose={handleApplyClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div>
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Confirmation
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Click on the Submit button if you confirm your application
+            </Typography>
+            <Button variant="contained" color="primary" onClick={handleApplySubmit}>
+              Submit
+            </Button>
+          </Box>
+        </div>
+      </Modal>
+
+
+      <Modal
+        open={openWithdraw}
+        onClose={handleWithdrawClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div>
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Withdraw Application
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Are you sure you want to withdraw your application?
+            </Typography>
+            <Button variant="contained" color="secondary" onClick={handleWithdrawSubmit}>
+              Withdraw
+            </Button>
+          </Box>
+        </div>
+      </Modal>
+
+
 
           <Button
             variant="contained"
@@ -134,7 +291,7 @@ const RoleListing = (props: Roles) => {
           </Button>
         </div>
       </div>
-    </div>
+    </div> 
   )
 }
 
