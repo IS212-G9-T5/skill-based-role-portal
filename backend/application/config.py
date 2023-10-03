56b2759144d00e3a350bec5f8cf5538377edbe89
Jaylin Lee <68802506+Jaylin0312@users.noreply.github.com>
2023-10-04 00:29:56 +0800

    feat: user authentication & authorisation (#15)

--- backend/application/config.py
+++ backend/application/config.py
@@ -7,8 +7,20 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = environ.get("JWT_SECRET_KEY")
+    JWT_TOKEN_LOCATION = ["cookies"]
+    JWT_COOKIE_SECURE = False
+    JWT_ACCESS_COOKIE_PATH = "/"
+    JWT_REFRESH_COOKIE_PATH = "/token/refresh"
+    JWT_COOKIE_CSRF_PROTECT = True
 
 
 class TestConfig:
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_TEST_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = environ.get("JWT_SECRET_KEY")
+    JWT_TOKEN_LOCATION = ["cookies"]
+    JWT_COOKIE_SECURE = False
+    JWT_ACCESS_COOKIE_PATH = "/"
+    JWT_REFRESH_COOKIE_PATH = "/token/refresh"
+    JWT_COOKIE_CSRF_PROTECT = True
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,20 +1,76 @@
 import type { RouteObject } from "react-router"
+import { Link } from "react-router-dom"
+
+import AccessControl from "./auth/AccessControl"
 import AllRoleListing from "./pages/applicant/AllRoleListing"
 import ViewRoleListing from "./pages/applicant/main"
 import CreateRoleListing from "./pages/hr/CreateRoleListing"
+import LoginForm from "./pages/login"
+
+const role = localStorage.getItem("role")
 
 const routes: RouteObject[] = [
   {
     path: "role-listing/:id",
-    element: <ViewRoleListing />,
+    element: (
+      <AccessControl
+        userPermissions={role ? role : ""}
+        allowedPermissions={["ADMIN", "USER", "MANAGER", "HR"]}
+        renderNoAccess={() => (
+          <div className="p-5">
+            You are not authenticated. Please proceed to {""}
+            <Link to="/" className="font-medium text-blue-600 hover:underline">
+              log in
+            </Link>
+          </div>
+        )}
+      >
+        <ViewRoleListing />
+      </AccessControl>
+    ),
   },
   {
     path: "all-role-listing",
-    element: <AllRoleListing />,
+    element: (
+      <AccessControl
+        userPermissions={role ? role : ""}
+        allowedPermissions={["ADMIN", "USER", "MANAGER", "HR"]}
+        renderNoAccess={() => (
+          <div className="p-5">
+            You are not authenticated. Please proceed to {""}
+            <Link to="/" className="font-medium text-blue-600 hover:underline">
+              log in
+            </Link>
+          </div>
+        )}
+      >
+        <AllRoleListing />
+      </AccessControl>
+    ),
   },
   {
     path: "create-role-listing",
-    element: <CreateRoleListing />,
+    element: (
+      <AccessControl
+        userPermissions={role ? role : ""}
+        allowedPermissions={["ADMIN"]}
+        renderNoAccess={() => (
+          <div className="p-5">
+            You do not have permission to access this page. Please proceed to{" "}
+            {""}
+            <Link to="/" className="font-medium text-blue-600 hover:underline">
+              log in
+            </Link>
+          </div>
+        )}
+      >
+        <CreateRoleListing />
+      </AccessControl>
+    ),
+  },
+  {
+    path: "/",
+    element: <LoginForm />,
   },
 ]
 
