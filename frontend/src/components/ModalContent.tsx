e26bdfad753bd8a2ddfdd8bc376725ffe21d3fd7
Christian Koh <111347751+christianmk04@users.noreply.github.com>
2023-10-11 15:18:08 +0800

    feat: Made View Individual Role Listings Dynamic and Added Viewing of Matched and Unmatched skills (#13)
    Co-authored-by: zhiweit <theanzhiwei@gmail.com>
    Co-authored-by: Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>

---
+++ frontend/src/components/ModalContent.tsx
@@ -0,0 +1,29 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+const ModalContent = ({ selectedChip }) => {
+    return (
+        <div>
+            <Box sx={style}>
+                <Typography variant="h4" component="h2" fontWeight="bold">{selectedChip?.name}</Typography>
+                <Typography variant="h6" sx={{ mt: 2 }}>{selectedChip?.description}</Typography>
+            </Box>
+        </div>
+        
+    );
+};
+
+export default ModalContent;
--- frontend/src/pages/applicant/RoleListing.tsx
+++ frontend/src/pages/applicant/RoleListing.tsx
@@ -1,27 +1,46 @@
-import { Button, Chip, Grid, Typography, Modal, Box } from "@mui/material"
+import { Button, Chip, Grid, Typography} from "@mui/material"
 import { useState , useEffect } from "react"
 import { useNavigate } from "react-router-dom"
-
+import Dialog from "@mui/material/Dialog"
+import DialogActions from "@mui/material/DialogActions"
+import DialogContent from "@mui/material/DialogContent"
+import DialogContentText from "@mui/material/DialogContentText"
+import DialogTitle from "@mui/material/DialogTitle"
+import { updateApplyRoleListing } from "../../api/RoleListingAPI"
 
 
 const RoleListing = (props: Roles) => {
+  
   const navigate = useNavigate()
 
-const handleBackToListings = () => {
-  navigate(`/all-role-listing`)
-}
+  const [selectedChip, setSelectedChip] = useState(props.userSkills[0]||null);
+  const [open, setOpen] = useState(false);
 
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+  const handleChipClick = (chip) => {
+    
+    // See if chip is in props.roleMatchData.skills_matched or props.roleMatchData.skills_unmatched
+    // If so, set selectedChip to chip and open modal
+    // Else, do nothing
+    const matchedSkill = props.roleMatchData.skills_matched.find(s => s.name === chip);
+    const unmatchedSkill = props.roleMatchData.skills_unmatched.find(s => s.name === chip);
+
+    if (matchedSkill) {
+      setSelectedChip(matchedSkill);
+      setOpen(true);
+    }
+    else if (unmatchedSkill) {
+      setSelectedChip(unmatchedSkill);
+      setOpen(true);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
+
+  const handleBackToListings = () => {
+    navigate(`/all-role-listing`)
+  }
 
 
   const [openApply, setOpenApply] = useState(false);
@@ -47,6 +66,11 @@ const style = {
   const handleApplySubmit = () => {
     // Handle the submission logic for applying here
     // This function should be replaced with your actual submission logic
+    updateApplyRoleListing(props.roleMatchData.has_applied,props.id);
+    console.log(props);
+    location.reload();
+
+
     console.log('Application submitted');
     setOpenApply(false);
   };
@@ -54,40 +78,17 @@ const style = {
   const handleWithdrawSubmit = () => {
     // Handle the submission logic for withdrawing here
     // This function should be replaced with your actual withdrawal logic
+    
+    updateApplyRoleListing(props.roleMatchData.has_applied,props.id);
+    console.log(props);
+    location.reload();
+
     console.log('Application withdrawn');
     setOpenWithdraw(false);
   };
-
-
-  const data = {
-    "listing": {
-        "end_date": "2023-10-11",
-        "id": 31,
-        "role": {
-            "description": "lorem ipsum",
-            "name": "Consultant",
-            "skills": [
-                "Collaboration",
-                "Communication",
-                "Learning Agility"
-            ]
-        },
-        "start_date": "2023-09-11",
-        "status": "OPEN"
-    },
-    "skills_match_count": 2,
-    "skills_match_pct": 0.67,
-    "skills_matched": [
-        "Communication",
-        "Collaboration"
-    ],
-    "skills_unmatched": [
-        "Learning Agility"
-    ],
-    "has_applied": false
-}
   
-  const has_applied = data.has_applied;
+
+  console.log(props)
 
   // useEffect(() => {
   //   const currUrl = window.location.href
@@ -100,6 +101,23 @@ const style = {
   //   })
   // }, [])
 
+  // useEffect(() => {
+  //   const currUrl = window.location.href
+  //   const id = currUrl.split("/").pop()
+
+  //   const fetchData = async () => {
+  //     try {
+  //       const data = await updateApplyRoleListing(id);
+  //       console.log(data);
+
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, [])
+
 
 
 
@@ -110,9 +128,18 @@ const style = {
           <strong>
             <Typography variant="h4">{props.name}</Typography>
           </strong>
-          <Typography variant="subtitle1" className="mb-[2%] text-[#B0B0B4]">
+
+          <Typography variant="subtitle1" className="mb-[2%] text-[#B0B0B4] py-1">
             Role ID: {props.id} | Status: {props.status} | Closing Date: {props.end_date}
           </Typography>
+          {
+            props.roleMatchData.has_applied ? (
+              <span className="rounded-full bg-green-500 text-white px-4 py-1">Applied</span>
+            ) 
+            : 
+            (null)
+          }
+
 
           <Typography variant="h6" gutterBottom style={{ marginTop: "3%" }}>
             <b>
@@ -129,24 +156,53 @@ const style = {
             <Typography variant="h6">
               <strong>
                 <span className="mr-2 bg-[#1976D2] pl-2"></span>
-                Skills Required
+                Skills Required [Matched Skills: {props.roleMatchData.skills_match_count}/{props.roleMatchData.skills_matched.length + props.roleMatchData.skills_unmatched.length}  ({props.roleMatchData.skills_match_pct*100}%)]
               </strong>
               <br></br>
               {props.skills.map((skill, index) => (
-                <Chip key={index} label={skill} className="mr-[1%] mt-[1%]" />
+                <Chip
+                key={index}
+                label={skill}
+                style={{margin:"5px"}}
+                className={`mr-[1%] mt-[1%] ${
+                  props.roleMatchData.skills_matched.find(
+                    (matchedSkill) => matchedSkill.name === skill
+                  )
+                    ? "bg-[#49d861] font-bold"
+                    : props.roleMatchData.skills_unmatched.find(
+                        (unmatchedSkill) => unmatchedSkill.name === skill
+                      )
+                    ? "bg-[#cff8db] opacity-50"
+                    : ""
+                }`}
+                onClick={() => handleChipClick(skill)}
+              />
               ))}
             </Typography>
           </Grid>
+          
+
+          <Dialog open={open} onClose={handleCloseModal}>
+            <DialogTitle id="alert-dialog-title">{selectedChip ? selectedChip.name : "No Chip Selected"}</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                {selectedChip ? selectedChip.description : "Please select a chip"}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleCloseModal}>Close</Button>
+            </DialogActions>
+          </Dialog>
 
 
           {
-            has_applied ? (
+            props.roleMatchData.has_applied ? (
               <Button
                 variant="contained"
-                color="secondary"  // You can use 'secondary' for a red color
+                color="secondary"
                 style={{ marginTop: "20px" }}
                 onClick={handleWithdrawOpen}
-                // onClick={() => }
+                disabled={props.status !== "OPEN"}
               >
                 Withdraw Application
               </Button>
@@ -156,6 +212,7 @@ const style = {
                 color="primary"
                 style={{ marginTop: "20px" }}
                 onClick={handleApplyOpen}
+                disabled={props.status !== "OPEN"}
               >
                 Apply Now
               </Button>
@@ -163,48 +220,29 @@ const style = {
           }
 
 
-      <Modal
-        open={openApply}
-        onClose={handleApplyClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <div>
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Confirmation
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Click on the Submit button if you confirm your application
-            </Typography>
-            <Button variant="contained" color="primary" onClick={handleApplySubmit}>
-              Submit
-            </Button>
-          </Box>
-        </div>
-      </Modal>
+      <Dialog open={openApply} onClose={handleApplyClose}>
+        <DialogTitle></DialogTitle>
+        <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+            Click on the Submit button if you confirm your application
+        </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color="primary" onClick={handleApplySubmit}>Submit</Button>
+        </DialogActions>
+      </Dialog>
 
-
-      <Modal
-        open={openWithdraw}
-        onClose={handleWithdrawClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <div>
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Withdraw Application
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Are you sure you want to withdraw your application?
-            </Typography>
-            <Button variant="contained" color="secondary" onClick={handleWithdrawSubmit}>
-              Withdraw
-            </Button>
-          </Box>
-        </div>
-      </Modal>
+      <Dialog open={openWithdraw} onClose={handleWithdrawClose}>
+        <DialogTitle></DialogTitle>
+        <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+            Are you sure you want to withdraw your application?
+        </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color="primary" onClick={handleWithdrawSubmit}>Submit</Button>
+        </DialogActions>
+      </Dialog>
 
 
 
@@ -215,8 +253,7 @@ const style = {
               marginTop: "20px",
               marginLeft: "10px",
             }}
-            onClick={handleBackToListings}
-          >
+            onClick={handleBackToListings}>
             Back to Listings
           </Button>
         </div>
