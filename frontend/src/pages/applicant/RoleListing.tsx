40b9eae68007a345dc0a46cb7cc8aa4f6c7acb8e
Christian Koh <111347751+christianmk04@users.noreply.github.com>
2023-10-11 15:18:08 +0800

    feat: Made View Individual Role Listings Dynamic and Added Viewing of Matched and Unmatched skills (#13)
    Co-authored-by: zhiweit <theanzhiwei@gmail.com>
    Co-authored-by: Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>

--- frontend/src/pages/applicant/RoleListing.tsx
+++ frontend/src/pages/applicant/RoleListing.tsx
@@ -91,23 +91,6 @@ const RoleListing = (props: Roles) => {
 
   console.log(props)
 
-  // useEffect(() => {
-  //   const currUrl = window.location.href
-  //   const id = currUrl.split("/").pop()
-
-  //   const fetchData = async () => {
-  //     try {
-  //       const data = await updateApplyRoleListing(id);
-  //       console.log(data);
-
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, [])
-
 
 
 
