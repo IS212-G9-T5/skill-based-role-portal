87bd0d309f03455db51469fefcbd7b4887633dae
Christian Koh <ckoh.2021@scis.smu.edu.sg>
2023-10-17 19:13:42 +0800

    feat: made navbar responsive

---
+++ frontend/src/components/DrawerComp.tsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import {Drawer, Button, Stack} from '@mui/material'
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import { IconButton } from '@material-ui/core';
+import { useNavigate } from "react-router-dom"
+import { Logout } from "../api/AuthAPI"
+
+const DrawerComp = ({links}) => {
+    const navigate = useNavigate()
+
+    function handleButtonClick(item) {
+        if (item.label === "Logout") {
+            Logout()
+            navigate("/")
+        } else {
+            navigate(item.to)
+        }
+    }
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
+                <Stack spacing={6} direction="column" sx={{padding:"20%"}}>
+                    {links.map((item, index) => (
+                        <Button color="inherit" key={index}>
+                        <b className="text-lg" onClick={() => handleButtonClick(item)} >
+                            {item.label}
+                        </b>
+                        </Button>
+                    ))}
+                </Stack>
+            </Drawer>
+
+            <IconButton color='inherit' onClick={() => setOpen(!open)}>
+                <MenuRoundedIcon />
+            </IconButton>
+        </>
+    )
+}
+
+export default DrawerComp
