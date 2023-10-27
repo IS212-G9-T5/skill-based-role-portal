a47169628359c6a071841071532dee8a54f76549
Wei Lun Teo <weilun.teo.2021@scis.smu.edu.sg>
2023-10-27 23:04:52 +0800

    set up cypress and apply role specs

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: '2y5zh4',
+  // projectId: 'frontend',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
---
+++ frontend/cypress/e2e/apply-role.cy.ts
@@ -0,0 +1,17 @@
+
+describe('Staff apply for role listing', () => {
+  it('Visits the desired URL', () => {
+    // Visit the initial URL
+    cy.visit('http://localhost:5173/');
+
+    // Enter staff ID and password and submit the form
+    cy.get('#staffId').type('140002');
+    cy.get('button[type="submit"]').click();
+
+    // cy.wait('@loginRequest').its('response.statusCode').should('eq', 200);
+    cy.url().should('include', '/all-role-listing?page=1&size=10');
+
+
+
+  });
+});
