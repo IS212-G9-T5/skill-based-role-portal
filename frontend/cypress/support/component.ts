ec73b7d480fdd9044b0bce95d9477727647fe791
Wei Lun Teo <weilun.teo.2021@scis.smu.edu.sg>
2023-10-29 20:49:08 +0800

    fix: add eslint disable

--- frontend/cypress/support/component.ts
+++ frontend/cypress/support/component.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 // ***********************************************************
 // This example support/component.ts is processed and
 // loaded automatically before your test files.
