e9a9da3f7c58450c03884c8297509aa7bb365bce
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2023-10-29 16:45:58 +0800

    fix: feature support for manager role

--- backend/application/routes/role_listing_route.py
+++ backend/application/routes/role_listing_route.py
@@ -19,7 +19,7 @@ from application.services import (
     staff_service,
 )
 from application.dto.response import ResponseBodyJSON
-from .route_decorators import admin_or_hr_required
+from .route_decorators import admin_or_hr_required, admin_or_hr_or_manager_required
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
 
@@ -55,7 +55,7 @@ def find_user_role_applications(id: int):
 
 @api.route("/available-listings", methods=["GET"])
 @jwt_required()
-@admin_or_hr_required()
+@admin_or_hr_or_manager_required
 def find_available_listings_paginated():
     page = request.args.get("page", 1, type=int)
     page_size = request.args.get("size", DEFAULT_PAGE_SIZE, type=int)
--- backend/application/routes/route_decorators.py
+++ backend/application/routes/route_decorators.py
@@ -26,3 +26,24 @@ def admin_or_hr_required():
         return decorator
 
     return wrapper
+
+
+def admin_or_hr_or_manager_required():
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            verify_jwt_in_request()
+            claims = get_jwt()
+            role = claims.get("role")
+            if (
+                role == AccessControlRole.Admin.value
+                or role == AccessControlRole.HR.value
+                or role == AccessControlRole.Manager.value
+            ):
+                return fn(*args, **kwargs)
+            else:
+                return jsonify(msg="You are not authorized to access this page."), 403
+
+        return decorator
+
+    return wrapper
