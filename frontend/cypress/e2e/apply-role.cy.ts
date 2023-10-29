5d011aeac00bae64203426d63eb2126c283d6f89
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2023-10-29 18:49:27 +0800

    fix: cypress test

--- frontend/cypress/e2e/apply-role.cy.ts
+++ frontend/cypress/e2e/apply-role.cy.ts
@@ -1,13 +1,21 @@
 describe("Staff apply for role listing", () => {
+  // Intercept the network request and alias it
+  beforeEach(() => {
+    cy.intercept('POST', '/api/login').as('loginRequest');
+  });
+
   it("Visits the desired URL", () => {
-    // Visit the initial URL
-    cy.visit("http://localhost:5173/")
+    cy.visit("http://localhost:5173/");
 
-    // Enter staff ID and password and submit the form
-    cy.get("#staffId").type("140002")
-    cy.get('button[type="submit"]').click()
+    cy.get("#staffId").type("140002");
+    cy.get('button[type="submit"]').click();
 
-    // cy.wait('@loginRequest').its('response.statusCode').should('eq', 200);
-    cy.url().should("include", "/all-role-listing?page=1&size=10")
-  })
-})
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 200);
+    
+    cy.window().then((window) => {
+      window.localStorage.setItem("userRole", "admin");
+    });
+
+    cy.url().should("include", "/all-role-listing");
+  });
+});
